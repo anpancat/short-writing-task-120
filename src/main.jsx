8f7370e2af3a08cbc6abc0edc6ad203c77042ad8
juyeon Lee <wndus618@naver.com>
2025-04-02 19:13:55 +0900

    🎉 Initial commit of WritingTest120 project

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import WritingTest from "./WritingTest"; // WritingTest.jsx 불러오기
+import App from "./App"; 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <WritingTest /> {/* WritingTest 컴포넌트 사용 */}
+    <App />
   </React.StrictMode>
 );
